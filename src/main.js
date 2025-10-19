dbb351b6f56c9a7925f12786fa601adac1de4244
Akamya <elodie.langlet@gmail.com>
2025-10-19 16:42:57 +0200

    feat: first draft responsive footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./main.scss";
+import "./mediaqueries.scss";
 
 document.querySelector("#app").innerHTML = `
   
